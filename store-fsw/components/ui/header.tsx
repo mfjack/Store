a01f7b7dffd80f9fa0a6e7d10dda1bbe76425e5a
mfjack <ferreira.marlon@live.com>
2024-02-20 13:16:11 -0300

    feat: add header menu using sheet component

--- store-fsw/components/ui/header.tsx
+++ store-fsw/components/ui/header.tsx
@@ -1,17 +1,55 @@
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import {
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  MenuIcon,
+  PercentIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 const Header = () => {
   return (
     <header>
       <Card className="flex items-center justify-between p-[1.875rem]">
-        <Button size="icon" variant="outline">
-          <MenuIcon />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader className="text-left text-lg font-semibold">
+              Menu
+            </SheetHeader>
+
+            <div className="mt-2 flex flex-col gap-3">
+              <Button variant="outline" className="w-full justify-start gap-2">
+                <LogInIcon size={15} />
+                Fazer Login
+              </Button>
+              <Button variant="outline" className="w-full justify-start gap-2">
+                <HomeIcon size={15} />
+                Ínicio
+              </Button>
+              <Button variant="outline" className="w-full justify-start gap-2">
+                <PercentIcon size={15} />
+                Ofertas
+              </Button>
+              <Button variant="outline" className="w-full justify-start gap-2">
+                <ListOrderedIcon size={15} />
+                Catálogo
+              </Button>
+            </div>
+          </SheetContent>
+        </Sheet>
+
         <h1 className="text-lg font-semibold">
           <span className="text-primary">FSW</span> Store
         </h1>
+
         <Button size="icon" variant="outline">
           <ShoppingCartIcon />
         </Button>
