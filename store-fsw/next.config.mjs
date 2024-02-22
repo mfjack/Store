9ddd666d1ea745402d5c663a1c7e506a57a6e349
mfjack <ferreira.marlon@live.com>
2024-02-22 12:39:09 -0300

    chore: add s3 domain to next config file

--- store-fsw/next.config.mjs
+++ store-fsw/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["store-fsw.s3.sa-east-1.amazonaws.com"],
+  },
+};
 
 export default nextConfig;
