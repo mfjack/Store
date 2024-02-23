27f13ce38b41bda03169e8625094479b914a8c07
mfjack <ferreira.marlon@live.com>
2024-02-23 16:36:49 -0300

    fix: ensure product prices are wrapped in one line

--- store-fsw/components/ui/productItem.tsx
+++ store-fsw/components/ui/productItem.tsx
@@ -8,12 +8,12 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex max-w-[156px] flex-col gap-4">
-      <div className="rounde-lg relative flex h-[170px] w-[156px] items-center justify-center bg-accent">
+    <div className="flex max-w-[170px] flex-col gap-4">
+      <div className="rounde-lg relative flex h-[170px] w-[170px] items-center justify-center bg-accent">
         <Image
           src={product.imageUrls[0]}
           alt={product.name}
-          height={0}  
+          height={0}
           width={0}
           sizes="100vw"
           className="h-auto max-h-[70%] w-auto max-w-[80%]"
@@ -34,15 +34,15 @@ const ProductItem = ({ product }: ProductItemProps) => {
         <div className="flex items-center gap-2">
           {product.discountPercentage > 0 ? (
             <>
-              <p className="font-semibold">
+              <p className="overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
                 R$ {product.totalPrice.toFixed(2)}
               </p>
-              <p className="text-xs line-through opacity-75">
+              <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
                 R$ {Number(product.basePrice).toFixed(2)}
               </p>
             </>
           ) : (
-            <p className="text-sm font-semibold">
+            <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold">
               R$ {product.basePrice.toFixed(2)}
             </p>
           )}
