75d229356469fa6e70eb7922809cdcb049001115
mfjack <ferreira.marlon@live.com>
2024-02-23 15:15:19 -0300

    feat: add second banner to home page

--- store-fsw/app/(home)/page.tsx
+++ store-fsw/app/(home)/page.tsx
@@ -15,18 +15,28 @@ const Home = async () => {
     <>
       <Image
         className="h-auto w-full p-5"
-        src="/banner01-home.png"
+        src="./banner01-home.svg"
         alt="Até 55% off esse mês"
-        width={350}
-        height={150}
+        width={100}
+        height={100}
       />
 
       <div className="p-5">
         <Categories />
       </div>
-      <div className="mt-8">
+
+      <div className="mt-5">
+        <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        className="h-auto w-full px-5"
+        src="./banner02-home.svg"
+        alt="Até 55% de desconto em mouses"
+        width={100}
+        height={100}
+      />
     </>
   );
 };
