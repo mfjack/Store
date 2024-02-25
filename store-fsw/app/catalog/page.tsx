63aa2fabd3a5bc680c1fecde04e7bede614bbc8c
mfjack <ferreira.marlon@live.com>
2024-02-25 19:08:22 -0300

    feat: add category page

--- store-fsw/app/catalog/page.tsx
+++ store-fsw/app/catalog/page.tsx
@@ -1,7 +1,22 @@
-const CatalogPage = () => {
+import { Badge } from "@/components/ui/badge";
+import { ShapesIcon } from "lucide-react";
+import CategoryItem from "./components/categoryItem";
+import { prismaClient } from "@/lib/prisma";
+
+const CatalogPage = async () => {
+  const categories = await prismaClient.category.findMany({});
   return (
-    <section>
-      <h1>Catalogo</h1>
+    <section className="flex flex-col gap-8 p-5">
+      <Badge
+        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+        variant="outline"
+      >
+        <ShapesIcon size={16} />
+        Catálogo
+      </Badge>
+      <div className="grid grid-cols-2 flex-wrap gap-8">
+        <CategoryItem key={category.id} category={category} />
+      </div>
     </section>
   );
 };
